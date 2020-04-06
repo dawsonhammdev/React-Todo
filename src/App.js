93607a2ebac168b7934880d36c8a81c25243b285
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-06 16:25:35 -0500

    I have added my todo data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
+const todo = [
+  {
+    task: 'Walk The Dog',
+    id: 123,
+    completed: false
+  },
+  {
+    task: 'Workout',
+    id: 123,
+    completed: false
+  },
+  {
+    task: 'Make Lunch',
+    id: 123,
+    completed: false
+  }
+]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>To Do Or Not To Do</h2>
       </div>
     );
   }
